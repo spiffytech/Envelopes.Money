305ebf59336968b3e227e744d6b10ebf65dbc6b6
Brian Cottingham <spiffytech@gmail.com>
2018-07-04 10:41:14 -0400

    Initialize page.js so forward/back buttons work

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,9 +1,12 @@
 import page from 'page';
 import Store from '../store';
 
+/* tslint:disable:no-console */
+
 export default function initRouter(store: typeof Store) {
   page('/', () => store.showHome())
   page('/login', () => store.showLogin())
+  page();
 }
 
 export function pushRoute(route: string) {

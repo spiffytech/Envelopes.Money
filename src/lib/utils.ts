dcddd0a2603665c286f22a354548d65b451f119a
Brian Cottingham <spiffytech@gmail.com>
2018-07-12 07:24:33 -0400

    Format currencies without a dollar sign so we don't have to deal with multi-currency feature requests. At least for now.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,6 @@ import * as Txns from './txns';
 export function formatCurrency(dollars: Txns.Dollars): string {
   return Intl.NumberFormat(
     navigator.language || 'en-US',
-    {style: 'currency', currency: 'USD'},
+    {minimumIntegerDigits: 1, minimumFractionDigits: 2, maximumFractionDigits: 2},
   ).format(dollars);
 }

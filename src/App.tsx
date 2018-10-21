f912c2d1e71a426c1c080d8a5c9cc7901e05f2d8
Brian Cottingham <spiffytech@gmail.com>
2018-10-21 13:02:26 +0000

    Added a HOC for guarding routes based on logged-in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import {inject, observer} from 'mobx-react';
 import * as React from 'react';
-// import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, RouteComponentProps} from 'react-router-dom';
+import * as ReactRouterDom from 'react-router-dom';
 
 import Store from './store';
+
+type RouteComponent = React.StatelessComponent<RouteComponentProps<{}>> | React.ComponentClass<any>
+function PrivateRoute(routeProps: {component?: RouteComponent, authed: boolean} & ReactRouterDom.RouteProps) {
+  const {component: Component, children, authed, ...rest} = routeProps;
+  return (
+    <Route
+      {...rest}
+      render={
+        /* tslint:disable-next-line:jsx-no-lambda */
+        (props) => {
+          if (!authed) return <Redirect to={{pathname: '/login', state: {from: props.location}}} />;
+          if (Component) return <Component {...props} />;
+          return children;
+        }
+      }
+    />
+  );
+}
 
 @inject('store')
 @observer
@@ -16,21 +35,25 @@ class App extends React.Component<{store?: Store}> {
           </div>
         </nav>
         <section className='section'>
-          <div className='container'>
-            <table>
-              <tbody>
-                {this.props.store!.transactionsByDate.map((txn) =>(
-                  <tr key={txn.id}>
-                    <td>{txn.date.toJSON()}</td>
-                    <td>{txn.payee}</td>
-                    <td>{txn.from.name} ⇨ {txn.to.map((to) => to.bucket.name).join(', ')}</td>
-                    <td>{txn.memo}</td>
-                    <td>{txn.amount.dollars}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <Router>
+            <PrivateRoute exact={true} path='/' authed={true}>
+              <div className='container'>
+                <table>
+                  <tbody>
+                    {this.props.store!.transactionsByDate.map((txn) =>(
+                      <tr key={txn.id}>
+                        <td>{txn.date.toJSON()}</td>
+                        <td>{txn.payee}</td>
+                        <td>{txn.from.name} ⇨ {txn.to.map((to) => to.bucket.name).join(', ')}</td>
+                        <td>{txn.memo}</td>
+                        <td>{txn.amount.dollars}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </PrivateRoute>
+          </Router>
         </section>
       </>
     );

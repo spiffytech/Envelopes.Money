3f1d6bb3386011744ff3aa5faa11fdcba3d5c12b
Brian Cottingham <spiffytech@gmail.com>
2018-10-22 11:24:48 +0000

    Transaction list: Replaced table cells with a custom element that's much more dense and tablet-friendly

--- src/views/Transactions.tsx
+++ src/views/Transactions.tsx
@@ -1,4 +1,4 @@
-import {inject, observer} from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import * as React from 'react';
 
 import * as utils from '../lib/utils';
@@ -6,18 +6,31 @@ import Store from '../store';
 
 @inject('store')
 @observer
-export default class Transactions extends React.Component<{store?: Store}> {
+export default class Transactions extends React.Component<{ store?: Store }> {
   public render() {
     return (
       <table className='table is-fullwidth'>
         <tbody>
-          {this.props.store!.transactionsByDate.map((txn) =>(
+          {this.props.store!.transactionsByDate.map((txn) => (
             <tr key={txn.id}>
-              <td style={{whiteSpace: 'nowrap'}}>{utils.formatDate(txn.date)}</td>
-              <td>{txn.payee}</td>
-              <td>{txn.from.name} ⇨ {txn.to.map((to) => to.bucket.name).join(', ')}</td>
-              <td>{txn.memo}</td>
-              <td>{utils.formatCurrency(txn.amount.dollars)}</td>
+              <td>
+                <div className='is-flex' style={{justifyContent: 'space-between'}}>
+                  <p className='is-flex'>
+                    <p className='has-text-weight-semibold'>{txn.payee}</p>
+                    {txn.payee && txn.memo ? <span>&nbsp;•&nbsp;</span> : null}
+                    <div className='has-text-weight-semibold is-size-7' style={{display: 'flex', alignItems: 'center'}}>
+                      {txn.memo}
+                    </div>
+                  </p>
+                  <p className='is-flex is-size-7'>
+                    <p>{utils.formatDate(txn.date)}</p>
+                  </p>
+                </div>
+                <div className='is-flex' style={{justifyContent: 'space-between'}}>
+                  <p>{utils.formatCurrency(txn.amount.dollars)}</p>
+                  <p className='is-size-7'>{txn.from.name} ⇨ {txn.to.map((to) => to.bucket.name).join(', ')}</p>
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>

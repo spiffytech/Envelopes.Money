4e1c447e76e85a6658151263c47e3449e001edc7
Brian Cottingham <spiffytech@gmail.com>
2019-01-27 02:14:52 +0000

    Account balances: Display as 'dollars'

--- client/src/components/AccountBalances.vue
+++ client/src/components/AccountBalances.vue
@@ -5,7 +5,7 @@
       <tr>Account Balances</tr>
       <tr v-for="balance in accountBalances" :key="balance.bucket.id">
         <td>{{balance.bucket.name}}</td>
-        <td>{{balance.balance}}</td>
+        <td class="number">{{balance.balance}}</td>
       </tr>
       <tr>Envelopes</tr>
       <tr v-for="balance in envelopeBalances" :key="balance.bucket.id">
@@ -16,19 +16,28 @@
 </template>
 
 <script lang="ts">
+import * as shades from 'shades';
 import Vue from 'vue';
 
+import {toDollars} from '@/lib/currency';
 import * as CommonTypes from '../../../common/lib/types';
+import * as AccountBalance from '../../../common/lib/AccountBalance';
 import Envelope from './Envelope.vue';
 
 export default Vue.extend({
   components: {Envelope},
 
   computed: {
-    accountBalances(): CommonTypes.AccountBalance {
-      return this.$store.getters['accounts/accountBalances'];
+    accountBalances(): CommonTypes.AccountBalance[] {
+
+      return shades.mod(shades.all(), 'balance')
+        ((amount: number) =>
+          (amount / 100).
+          toLocaleString([], {minimumFractionDigits: 2, maximumFractionDigits: 2}) as any as number
+        )
+        (this.$store.getters['accounts/accountBalances']);
     },
-    envelopeBalances(): CommonTypes.AccountBalance {
+    envelopeBalances(): CommonTypes.AccountBalance[] {
       return this.$store.getters['accounts/envelopeBalances'];
     },
   },
---
+++ common/lib/AccountBalance.ts
@@ -0,0 +1,9 @@
+import * as shades from 'shades';
+import {Lens} from 'shades/types/utils';
+
+import {IBucket} from './types';
+
+export interface T {
+  bucket: IBucket;
+  balance: number;
+}

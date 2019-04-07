80f0e237f070965f96da35aa5a255119fc70379d
spiffytech <spiffy@spiffy.tech>
2019-04-07 19:47:43 -0400

    Use a build-specific cache suffix so we can invalidate our cache when we deploy new builds of the app. Also clean up the cache so we don't leave garbage around.

--- client/src/lib/cache.ts
+++ client/src/lib/cache.ts
@@ -1,12 +1,28 @@
 import localForage from 'localforage';
+import preval from 'preval.macro';
+
+// This is used to apply a build-specific suffix to our cache keys, so if we
+// deploy a new app version the cache is invalidated and we don't load old
+// values with legacy data structures
+const suffix = preval`module.exports = new Date().getTime()`;
 
 export async function cache(key: string, data: any) {
   await localForage.setItem(key, JSON.stringify(data));
   return;
 }
 
+async function cleanUpCache() {
+  const keysToPurge =
+    (await localForage.keys()).
+    filter((key) => key.startsWith('cache/') && !key.endsWith(suffix));
+
+  await Promise.all((keysToPurge.map((key) => localForage.removeItem(key))));
+}
+
 export async function withCache<T>(key: string, fn: () => Promise<T>, onFetch: (t: T, isFresh: boolean) => void) {
-  const fullKey = `cache/${key}`;
+  cleanUpCache();
+
+  const fullKey = `cache/${key}-${suffix}`;
   const {staleP, freshP} = {
     staleP: localForage.getItem<string>(fullKey).then((data) => data ? JSON.parse(data) : data),
     freshP: fn().then(async (data) => {

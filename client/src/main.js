a28168513af79c1070cd73bfa2bc7984ff98f4cc
spiffytech <spiffy@spiffy.tech>
2019-07-14 08:21:48 -0400

    Enable LogRocket (screen capture is disabled)

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,24 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
-import * as accountsStore from './stores/accounts';
-import * as Envelope from './lib/Envelope';
+import * as accountsStore from "./stores/accounts";
+import * as Envelope from "./lib/Envelope";
+import LogRocket from "logrocket";
 
 if (window.Cypress) {
-	window.accountsStore = accountsStore;
-	window.Envelope = Envelope;
+  window.accountsStore = accountsStore;
+  window.Envelope = Envelope;
 }
 
 const app = new App({
-	target: document.body,
-	props: {},
+  target: document.body,
+  props: {}
 });
 
 export default app;
+
+if (process.env.LOGROCKET_APP) {
+  LogRocket.init(process.env.LOGROCKET_APP, {
+    network: { isEnabled: false },
+    dom: { isEnabled: false }
+  });
+}

ea1b57183a6aff8896e3907a97650ccdaf9181a9
spiffytech <spiffy@spiffy.tech>
2019-07-19 21:11:21 -0400

    Move Transaction out into its own component so we can use it elsewhere

--- client/src/Home.svelte
+++ client/src/Home.svelte
@@ -3,6 +3,7 @@
   import page from 'page';
 
   import Accounts from './Accounts.svelte';
+  import Transaction from './components/Transaction.svelte';
   import {toDollars} from './lib/pennies';
   import {arrays as derivedStore, store} from './stores/main';
 
@@ -65,32 +66,7 @@
   {/if}
 
   {#each txnsGrouped.slice(page * numItemsPerPage, (pageNum+1) * numItemsPerPage) as txn}
-    <div
-      on:click={() => page(`/editTxn/${txn.txn_id}`)}
-      class="flex justify-between p-3 border border-grey-light rounded mb-1"
-      data-cy='transaction'
-    >
-      <div class="mr-2">{txn.date}</div>
-      <div class="text-left flex-1 min-w-0 mr-2">
-        <div class="text-left font-bold" data-cy='transaction-label'>
-          {#if txn.label}{txn.label}{:else}<span class='italic text-sm'>No Label</span>{/if} 
-          {#if txn.memo}<span title={txn.memo}>((Memo))</span>{/if}
-        </div>
-
-        <div class="flex flex-1 text-xs italic">
-          <span class="whitespace-no-wrap">{txn.from_name}</span>
-          &nbsp;→&nbsp;
-          <span
-            style="text-overflow: ellipsis"
-            class="whitespace-no-wrap overflow-hidden"
-          >
-            {txn.to_names}
-          </span>
-        </div>
-      </div>
-
-      <div class="text-right">{toDollars(txn.amount)}</div>
-    </div>
+    <Transaction {txn} />
   {/each}
 
   {#each new Array(Math.ceil(txnsGrouped.length / numItemsPerPage)).fill(null).map((_, i) => i) as btn_number}
---
+++ client/src/components/Transaction.svelte
@@ -0,0 +1,37 @@
+<script>
+  import {toDollars} from '../lib/pennies';
+
+  export let txn;
+</script>
+
+<a style="display: contents" href={`/editTxn/${txn.txn_id}`} class="no-underline text-black">
+  <div
+    class="flex justify-between p-3 border border-grey-light rounded mb-1"
+    data-cy="transaction">
+    <div class="mr-2">{txn.date}</div>
+    <div class="text-left flex-1 min-w-0 mr-2">
+      <div class="text-left font-bold" data-cy="transaction-label">
+        {#if txn.label}
+          {txn.label}
+        {:else}
+          <span class="italic text-sm">No Label</span>
+        {/if}
+        {#if txn.memo}
+          <span title={txn.memo}>((Memo))</span>
+        {/if}
+      </div>
+
+      <div class="flex flex-1 text-xs italic">
+        <span class="whitespace-no-wrap">{txn.from_name}</span>
+        &nbsp;→&nbsp;
+        <span
+          style="text-overflow: ellipsis"
+          class="whitespace-no-wrap overflow-hidden">
+          {txn.to_names}
+        </span>
+      </div>
+    </div>
+
+    <div class="text-right">{toDollars(txn.amount)}</div>
+  </div>
+</a>

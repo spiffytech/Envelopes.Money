5267d0fb842daf54807d499dec660eb9dbb1332b
spiffytech <spiffy@spiffy.tech>
2019-07-25 09:00:23 -0400

    Actually get Docker to read the env vars enabling couch/pouch

--- docker-start.sh
+++ docker-start.sh
@@ -1,5 +1,5 @@
 set -eu
 set -o pipefail
 
-node dist/server/src/scripts/env2window.js GRAPHQL_HTTP_HOST,GRAPHQL_WSS_HOST,LOGROCKET_APP > ../client/public/env.js
+node dist/server/src/scripts/env2window.js GRAPHQL_HTTP_HOST,GRAPHQL_WSS_HOST,LOGROCKET_APP,USE_POUCH,COUCHDB > ../client/public/env.js
 node dist/server/index.js

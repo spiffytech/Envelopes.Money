880f0bd0318083e68ed51f01d18d594d361b9145
spiffytech <spiffy@spiffy.tech>
2019-07-26 17:27:39 -0400

    Formatted libpouch

--- client/src/lib/pouch.js
+++ client/src/lib/pouch.js
@@ -1,11 +1,11 @@
-import Debug from 'debug';
-import immer from 'immer';
+import Debug from "debug";
+import immer from "immer";
 import PouchDB from "pouchdb";
 import PouchDBAuthenticaton from "pouchdb-authentication";
 import PouchDBFind from "pouchdb-find";
 import PouchDBUpsert from "pouchdb-upsert";
 
-const debug = Debug('Envelopes.Money:pouch');
+const debug = Debug("Envelopes.Money:pouch");
 window.PouchDB = PouchDB;
 
 PouchDB.plugin(PouchDBAuthenticaton);
@@ -13,126 +13,167 @@ PouchDB.plugin(PouchDBFind);
 PouchDB.plugin(PouchDBUpsert);
 
 function toHex(str) {
-  var result = "";
-  for (var i = 0; i < str.length; i++) {
-    result += str.charCodeAt(i).toString(16);
-  }
-  return result;
+    var result = "";
+    for (var i = 0; i < str.length; i++) {
+        result += str.charCodeAt(i).toString(16);
+    }
+    return result;
 }
 
 const txnIdIndex = {
-  index: { fields: ["txn_id"] },
-  ddoc: "txns_txn_id_index"
+    index: { fields: ["txn_id"] },
+    ddoc: "txns_txn_id_index"
 };
 const recordTypeIndex = {
-  index: { fields: ["type_"] },
-  ddoc: "record_type_index"
+    index: { fields: ["type_"] },
+    ddoc: "record_type_index"
 };
 
 export function initMetaDB() {
-  if (!window._env_.USE_POUCH) return;
-  return new PouchDB('meta');
+    if (!window._env_.USE_POUCH) return;
+    return new PouchDB("meta");
 }
 
 export default function init(username) {
-  if (!window._env_.USE_POUCH) return;
-  const localDB = new PouchDB("envelopes.money");
-  const remoteDB = window._env_.COUCHDB
-    ? new PouchDB(window._env_.COUCHDB + "/userdb-" + toHex(username), {
-        skip_setup: true
-      })
-    : null;
-  localDB.remoteDB = remoteDB;
+    if (!window._env_.USE_POUCH) return;
+    const localDB = new PouchDB("envelopes.money");
+    const remoteDB = window._env_.COUCHDB
+        ? new PouchDB(window._env_.COUCHDB + "/userdb-" + toHex(username), {
+              skip_setup: true
+          })
+        : null;
+    localDB.remoteDB = remoteDB;
 
-  localDB.createIndex(txnIdIndex).catch(console.error);
-  localDB.createIndex(recordTypeIndex).catch(console.error);
+    localDB.createIndex(txnIdIndex).catch(console.error);
+    localDB.createIndex(recordTypeIndex).catch(console.error);
 
-  window.localDB = localDB;
-  return localDB;
+    window.localDB = localDB;
+    return localDB;
 }
 
-export function logIn(localDB, {email, password}) {
-  return localDB.remoteDB.logIn(email, password);
+export function logIn(localDB, { email, password }) {
+    return localDB.remoteDB.logIn(email, password);
 }
 
-export function signUp(localDB, {email, password}) {
-  return localDB.remoteDB.signUp(email, password);
+export function signUp(localDB, { email, password }) {
+    return localDB.remoteDB.signUp(email, password);
 }
 
 export function sync(localDB, pouchStore) {
-  debug("Setting up PouchDB replication");
-  localDB.sync(localDB.remoteDB, {live: true, retry: true, heartbeat: 10000}).
-  on('paused', err => pouchStore.update($store => immer($store, s => ({...s, state: 'paused', stateDetail: err})))).
-  on('error', err => pouchStore.update($store => immer($store, s => ({...s, state: 'error', stateDetail: err})))).
-  on('active', () => pouchStore.update($store => immer($store, s => ({...s, state: 'active', stateDetail: null})))).
-  on('complete', info => { throw new Error(`PouchDB replication unexpectedly completed. info is ${info}`)}).
-  on('denied', err => { throw new Error(`PouchDB replication was denied. Error is ${err}`)});
-
+    debug("Setting up PouchDB replication");
+    localDB
+        .sync(localDB.remoteDB, { live: true, retry: true, heartbeat: 10000 })
+        .on("paused", err =>
+            pouchStore.update($store =>
+                immer($store, s => ({
+                    ...s,
+                    state: "paused",
+                    stateDetail: err
+                }))
+            )
+        )
+        .on("error", err => {
+            debug('Got error code %s', err.status);
+            pouchStore.update($store =>
+                immer($store, s => ({ ...s, state: "error", stateDetail: err }))
+            );
+        })
+        .on("active", () =>
+            pouchStore.update($store =>
+                immer($store, s => ({
+                    ...s,
+                    state: "active",
+                    stateDetail: null
+                }))
+            )
+        )
+        .on("complete", info => {
+            throw new Error(
+                `PouchDB replication unexpectedly completed. info is ${info}`
+            );
+        })
+        .on("denied", err => {
+            throw new Error(`PouchDB replication was denied. Error is ${err}`);
+        });
 }
 
 export class PouchTransactions {
-  constructor(localDB) {
-    this.localDB = localDB;
-  }
+    constructor(localDB) {
+        this.localDB = localDB;
+    }
 
-  async loadAll() {
-    const results = await this.localDB.find({
-      selector: { type_: "transaction" },
-      index: "record_type_index"
-    });
-    return results.docs;
-  }
+    async loadAll() {
+        const results = await this.localDB.find({
+            selector: { type_: "transaction" },
+            index: "record_type_index"
+        });
+        return results.docs;
+    }
 
-  /**
-   * @param {ITransaction[]} txns
-   */
-  async saveAll(txns) {
-    const existingTxnsInGroup = await this.localDB.find({selector: {txn_id: txns[0].txn_id}});
-    debug('Found existing txns: %O', existingTxnsInGroup);
-    await Promise.all(existingTxnsInGroup.docs.map((doc) => this.localDB.remove(doc)));
+    /**
+     * @param {ITransaction[]} txns
+     */
+    async saveAll(txns) {
+        const existingTxnsInGroup = await this.localDB.find({
+            selector: { txn_id: txns[0].txn_id }
+        });
+        debug("Found existing txns: %O", existingTxnsInGroup);
+        await Promise.all(
+            existingTxnsInGroup.docs.map(doc => this.localDB.remove(doc))
+        );
 
-    await Promise.all(
-      txns.map(txn =>
-        this.localDB.upsert(txn.id.replace(/^_+/, "☃︎"), ({ _rev }) => ({
-          ...txn,
-          _id: txn.id,
-          type_: 'transaction',
-          _rev
-        }))
-      )
-    );
-  }
+        await Promise.all(
+            txns.map(txn =>
+                this.localDB.upsert(
+                    txn.id.replace(/^_+/, "☃︎"),
+                    ({ _rev }) => ({
+                        ...txn,
+                        _id: txn.id,
+                        type_: "transaction",
+                        _rev
+                    })
+                )
+            )
+        );
+    }
 
-  /**
-   * @param {string} txn_group_id
-   */
-  async delete(txn_group_id) {
-    const txnsByGroupId = await this.localDB.find({
-      selector: {
-        txn_id: txn_group_id
-      },
-      use_index: "txns_txn_id_index"
-    });
+    /**
+     * @param {string} txn_group_id
+     */
+    async delete(txn_group_id) {
+        const txnsByGroupId = await this.localDB.find({
+            selector: {
+                txn_id: txn_group_id
+            },
+            use_index: "txns_txn_id_index"
+        });
 
-    await Promise.all(txnsByGroupId.docs.map(txn => this.localDB.remove(txn)));
-  }
+        await Promise.all(
+            txnsByGroupId.docs.map(txn => this.localDB.remove(txn))
+        );
+    }
 }
 
 export class PouchAccounts {
-  constructor(localDB) {
-    this.localDB = localDB;
-  }
+    constructor(localDB) {
+        this.localDB = localDB;
+    }
 
-  async loadAll() {
-    const results = await this.localDB.find({
-      selector: { type_: "account" },
-      index: "record_type_index"
-    });
-    return results.docs;
-  }
+    async loadAll() {
+        const results = await this.localDB.find({
+            selector: { type_: "account" },
+            index: "record_type_index"
+        });
+        return results.docs;
+    }
 
-  async save(account) {
-    debug('Saving account %O', account);
-    await this.localDB.upsert(account.id, ({_rev}) => ({...account, _id: account.id, type_: 'account', _rev}));
-  }
+    async save(account) {
+        debug("Saving account %O", account);
+        await this.localDB.upsert(account.id, ({ _rev }) => ({
+            ...account,
+            _id: account.id,
+            type_: "account",
+            _rev
+        }));
+    }
 }

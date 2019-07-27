970af9baac9e7844a43853596476a097dfc3f1da
spiffytech <spiffy@spiffy.tech>
2019-07-27 12:47:49 -0400

    Add an option to alert on any browser JS errors

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 import Debug from 'debug';
-import LogRocket from "logrocket";
+import LogRocket from 'logrocket';
 
-import App from "./App.svelte";
-import * as accountsStore from "./stores/accounts";
-import * as Envelope from "./lib/Envelope";
-import {initMetaDB} from './lib/pouch';
+import App from './App.svelte';
+import * as accountsStore from './stores/accounts';
+import * as Envelope from './lib/Envelope';
+import { initMetaDB } from './lib/pouch';
 
 const debug = Debug('Envelopes.Money:main');
+
+if (window._env_.ALERT_ON_ERROR) {
+  window.onerror = err => alert(err);
+}
 
 async function main() {
   if (window._env_.POUCH_ONLY && !window._env_.USE_POUCH) {
@@ -26,14 +30,16 @@ async function main() {
       if (ex.status !== 404) {
         alert(ex.message);
       }
-      debug('Didn\'t find the credentials in the DB');
+      debug("Didn't find the credentials in the DB");
     }
   }
 
   if (!window._env_.POUCH_ONLY) {
     try {
-      const response = await axios.get("/api/credentials", { withCredentials: true })
-      creds = {...(creds || {}), ...response.data};
+      const response = await axios.get('/api/credentials', {
+        withCredentials: true,
+      });
+      creds = { ...(creds || {}), ...response.data };
     } catch (ex) {
       creds = null;
     }
@@ -46,13 +52,13 @@ async function main() {
 
   new App({
     target: document.body,
-    props: { creds }
+    props: { creds },
   });
 
   if (window._env_.LOGROCKET_APP) {
     LogRocket.init(window._env_.LOGROCKET_APP, {
       network: { isEnabled: false },
-      dom: { isEnabled: false }
+      dom: { isEnabled: false },
     });
   }
 }
@@ -61,10 +67,13 @@ main();
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
   });
 }

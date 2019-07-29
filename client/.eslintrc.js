91c7e2b1453556ddbbe63d57840945682961b094
spiffytech <spiffy@spiffy.tech>
2019-07-29 17:20:10 -0400

    ESLint: Allow CouchDB 'emit', console.error

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "cypress/globals": true
-    },
-    "plugins": [
-        "svelte3",
-        "cypress"
-    ],
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+    'cypress/globals': true,
+  },
+  plugins: ['svelte3', 'cypress'],
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    emit: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {'no-console': ["error", { allow: ["warn", "error"] }]},
 };

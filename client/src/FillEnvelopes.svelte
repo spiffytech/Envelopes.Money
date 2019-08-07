8dc8213aac20bd29fc73d451b9f9258ef0eb4b6a
spiffytech <spiffy@spiffy.tech>
2019-08-07 18:04:23 -0400

    Rewrote FillEnvelopes to be more compact, easier to work with, and less brittle to fill out

--- client/src/FillEnvelopes.svelte
+++ client/src/FillEnvelopes.svelte
@@ -1,4 +1,6 @@
 <script>
+  import always from 'ramda/es/always';
+  import fromPairs from 'ramda/es/fromPairs';
   import page from 'page';
   import * as shortid from 'shortid';
   import { getContext } from 'svelte';
@@ -9,107 +11,143 @@
   import { toDollars } from './lib/pennies';
   import * as Transactions from './lib/Transactions';
   import { formatDate } from './lib/utils';
+  import saveTransactions from './lib/transactions/saveTransactions';
 
   const accountsStore = getContext('accountsStore');
+  const transactionsStore = getContext('transactionsStore');
+  const dexie = getContext('dexie');
   const balancesStore = getContext('balancesStore');
   const intervalStore = getContext('intervalStore');
 
-  let fills = $accountsStore.filter(account => account.type === 'envelope').map(envelope => ({
-    amount: 0,
-    envelope: { ...envelope, balance: $balancesStore[envelope.id] },
-  }));
-  $: sumOfFills = fills
-    .map(fill => fill.amount)
-    .reduce((acc, item) => acc + item, 0);
-  $: unallocated = fills.find(fill => fill.envelope.name === '[Unallocated]');
-  $: notUnallocated = fills.filter(
-    fill => fill.envelope.name !== '[Unallocated]'
+  const envelopes = $accountsStore.filter(
+    account => account.type === 'envelope'
+  );
+  $: envelopesMap = new Map(envelopes.map(envelope => [envelope.id, envelope]));
+
+  const fillsObj = new fromPairs(
+    envelopes.map(envelope => [envelope.id, null])
   );
 
-  $: c = fill =>
-    Math.round(Balances.calcAmountForPeriod(fill.envelope)[$intervalStore]);
+  $: unallocated = envelopes.find(
+    envelope => envelope.name === '[Unallocated]'
+  );
+
+  $: calcFillAmount = envelope => {
+    const envelopeWithBalance = {
+      ...envelope,
+      balance: $balancesStore[envelope.id],
+    };
+    return Math.round(
+      Balances.calcAmountForPeriod(envelopeWithBalance)[$intervalStore]
+    );
+  };
+
+  function setToFillAmount(envelope, interval, balance) {
+    const fillAmount = calcFillAmount(envelope);
+    return -balance + fillAmount;
+  }
+  function setToZero(envelope, balance) {
+    return -balance;
+  }
+
+  $: fills = Object.entries(fillsObj).map(([envelopeId, fillType]) => {
+    const envelope = envelopesMap.get(envelopeId);
+    switch (fillType) {
+      case null:
+        return [envelopeId, 0];
+        break;
+      case 'fill-with-amount':
+        return [envelopeId, calcFillAmount(envelope)];
+        break;
+      case 'set-to-amount':
+        return [
+          envelopeId,
+          setToFillAmount(envelope, $intervalStore, $balancesStore[envelopeId]),
+        ];
+        break;
+      case 'set-to-zero':
+        return [envelopeId, setToZero(envelope, $balancesStore[envelopeId])];
+        break;
+    }
+  });
+
+  $: sumOfFills = fills
+    .map(([ignored, fillAmount]) => fillAmount)
+    .reduce((acc, item) => acc + item, 0);
 
   async function handleSubmit() {
-    const txnId = shortid.generate();
+    const txnGroupId = shortid.generate();
     const txns = fills
-      .map((fill, i) => {
+      .map(([envelopeId, fillAmount], i) => {
         const txn = {
           id: `transaction/${shortid.generate()}`,
           memo: '',
           date: new Date(),
-          amount: fill.amount,
+          amount: fillAmount,
           label: null,
           type: 'envelopeTransfer',
-          txn_id: txnId,
-          from_id: unallocated.envelope.id,
-          to_id: fill.envelope.id,
+          txn_id: txnGroupId,
+          from_id: unallocated.id,
+          to_id: envelopeId,
         };
         return txn;
       })
       .filter(txn => txn.amount !== 0);
 
-      alert('Saving to Kinto is not implemented!');
+    await saveTransactions({transactionsStore}, dexie, txns);
     page('/home');
   }
 </script>
 
-<div class="flex justify-around content">
-  <form
-    on:submit|preventDefault={handleSubmit}
-    class="bg-white p-4 rounded border border-2 border-gray-400 m-4">
-    <p>Unallocated: {toDollars(unallocated.envelope.balance - sumOfFills)}</p>
+<form class="content" on:submit|preventDefault={handleSubmit}>
+  <p>Unallocated: {toDollars($balancesStore[unallocated.id] - sumOfFills)}</p>
 
-    <select bind:value={$intervalStore} on:change={(event) => intervalStore.set(event.target.value)} class="border">
-      <option value="weekly">Weekly</option>
-      <option value="biweekly">Biweekly</option>
-      <option value="bimonthly">Bimonthly</option>
-      <option value="monthly">Monthly</option>
-      <option value="annually">Annually</option>
-    </select>
+  <select
+    bind:value={$intervalStore}
+    on:change={event => intervalStore.set(event.target.value)}
+    class="border">
+    <option value="weekly">Weekly</option>
+    <option value="biweekly">Biweekly</option>
+    <option value="bimonthly">Bimonthly</option>
+    <option value="monthly">Monthly</option>
+    <option value="annually">Annually</option>
+  </select>
 
-    {#each notUnallocated as fill}
-      <div class="flex justify-between bubble">
-        <div>
-          <div>
-            <Balance account={fill.envelope} defaultDaysToRender={15} />
-
-            <button
-              class="border btn btn-secondary"
-              on:click|preventDefault={() => (fill.amount = -fill.envelope.balance + c(fill))}>
-              Set to {toDollars(c(fill))}
-            </button>
-          </div>
-
-          <div>
-            <button
-              class="border btn btn-secondary"
-              on:click|preventDefault={() => (fill.amount = c(fill))}>
-              Fill with {toDollars(c(fill))}
-            </button>
-          </div>
-
-          <div>
-            <button
-              class="border btn btn-secondary"
-              on:click|preventDefault={() => (fill.amount = -fill.envelope.balance)}>
-              Set to 0
-            </button>
-          </div>
-
-          <div>
-            <p>Old Balance: {toDollars(fill.envelope.balance)}</p>
-            <span>Fill:</span>
-            <MoneyInput
-              amount={fill.amount}
-              defaultType="credit"
-              on:change={({ detail }) => (fill.amount = detail)} />
-            <p>{fill.amount}</p>
-            <p>New Balance: {toDollars(fill.envelope.balance + fill.amount)}</p>
-          </div>
-        </div>
+  {#each envelopes as envelope (envelope.id)}
+    <div class="flex flex-wrap">
+      <div style="flex-basis: 450px;">
+        <Balance account={envelopesMap.get(envelope.id)} defaultDaysToRender={15} />
       </div>
-    {/each}
+      <div>
+        <label>
+          <input
+            type="radio"
+            bind:group={fillsObj[envelope.id]}
+            value="fill-with-amount" />
+          Fill with {toDollars(calcFillAmount(envelope))} ({$intervalStore} amount)
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            bind:group={fillsObj[envelope.id]}
+            value="set-to-amount" />
+          Raise/lower to {toDollars(calcFillAmount(envelope))} ({$intervalStore}
+          amount)
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            bind:group={fillsObj[envelope.id]}
+            value="set-to-zero" />
+          Set to 0.00
+        </label>
+      </div>
+    </div>
 
-    <input type="submit" value="Fill!" class="btn btn-primary" />
-  </form>
-</div>
+    <hr />
+  {/each}
+
+  <input type="submit" value="Fill!" class="btn btn-primary" />
+</form>

a0800b383f921bee44129fa53c26966643fa6d2d
spiffytech <spiffy@spiffy.tech>
2019-08-11 21:18:22 -0400

    Actually save the fingerprint when we save account/transactions

--- client/src/lib/accounts/saveAccount.js
+++ client/src/lib/accounts/saveAccount.js
@@ -1,18 +1,24 @@
+import bencode from 'bencode';
 import immer from 'immer';
+import shajs from 'sha.js';
 
 /**
  * Given some accounts that changed, save them to the DB and update the acconut
  * store
  */
-export default async function saveTags(
-  { accountsStore },
-  dexie,
-  account
-) {
-  await dexie.accounts.put(account);
-  accountsStore.update($accounts => immer($accounts, draft => {
-    const index = draft.findIndex(a => a.id === account.id);
-    draft[index] = account;
-  }));
-}
+export default async function saveTags({ accountsStore }, dexie, account) {
+  const accountWithFingerprint = {
+    ...account,
+    _fingerprint: shajs('sha256')
+      .update(bencode.encode(JSON.stringify(account)))
+      .digest('hex'),
+  };
 
+  await dexie.accounts.put(accountWithFingerprint);
+  accountsStore.update($accounts =>
+    immer($accounts, draft => {
+      const index = draft.findIndex(a => a.id === accountWithFingerprint.id);
+      draft[index] = accountWithFingerprint;
+    })
+  );
+}
--- client/src/lib/transactions/saveTransactions.js
+++ client/src/lib/transactions/saveTransactions.js
@@ -1,6 +1,8 @@
+import bencode from 'bencode';
 import comparator from 'ramda/es/comparator';
 import identity from 'ramda/es/identity';
 import immer from 'immer';
+import shajs from 'sha.js';
 import { get as storeGet } from 'svelte/store';
 
 export default async function saveTransactions(
@@ -8,15 +10,24 @@ export default async function saveTransactions(
   dexie,
   newTxns
 ) {
+  const txnsWithFingerprint = newTxns.map(txn => ({
+    ...txn,
+    _fingerprint: shajs('sha256')
+      .update(bencode.encode(JSON.stringify(txn)))
+      .digest('hex'),
+  }));
+
   const existingTxns = storeGet(transactionsStore).filter(
-    txn => txn.txn_id === newTxns[0].txn_id
+    txn => txn.txn_id === txnsWithFingerprint[0].txn_id
   );
   const toDelete = existingTxns.filter(
-    txn => newTxns.findIndex(t => t.id === txn.id) === -1
+    txn => txnsWithFingerprint.findIndex(t => t.id === txn.id) === -1
   );
 
   await Promise.all(toDelete.map(txn => dexie.transactions.delete(txn.id)));
-  await Promise.all(newTxns.map(txn => dexie.transactions.put(txn)));
+  await Promise.all(
+    txnsWithFingerprint.map(txn => dexie.transactions.put(txn))
+  );
 
   transactionsStore.set(
     immer(

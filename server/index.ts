487a99c13534c749be6f4a499c67f2e35cda402e
spiffytech <spiffy@spiffy.tech>
2019-11-02 17:41:36 -0400

    Stop wrapping server code in 'pouch only' conditional. Pouch is dead to us.

--- server/index.ts
+++ server/index.ts
@@ -46,112 +46,106 @@ app.use(
   })
 );
 
-if (!process.env.POUCH_ONLY) {
-  app.use("/auth", unauth);
+app.use("/auth", unauth);
 
-  const authedRouter = express.Router();
-  authedRouter.use(async (req, res, next) => {
-    const apikey = sessions.apikeyFromRequest(req);
-    if (!apikey) {
-      res.statusCode = 401;
-      console.log("No API key in request");
-      return res.send({ error: "unauthorized" });
-    }
+const authedRouter = express.Router();
+authedRouter.use(async (req, res, next) => {
+  const apikey = sessions.apikeyFromRequest(req);
+  if (!apikey) {
+    res.statusCode = 401;
+    console.log("No API key in request");
+    return res.send({ error: "unauthorized" });
+  }
 
-    const session = await sessions.lookUpSession(apikey);
-    if (!session) {
-      res.statusCode = 401;
-      console.error("No session for that API key");
-      return res.send({ error: "unauthorized" });
-    }
+  const session = await sessions.lookUpSession(apikey);
+  if (!session) {
+    res.statusCode = 401;
+    console.error("No session for that API key");
+    return res.send({ error: "unauthorized" });
+  }
 
-    req.userId = session.id;
-    req.apikey = apikey;
-    return next();
+  req.userId = session.id;
+  req.apikey = apikey;
+  return next();
+});
+
+authedRouter.get("/credentials", (req, res) => {
+  res.json({
+    email: req.session!.credentials.email,
+    password: req.session!.credentials.password,
+    apikey: req.session!.credentials.apikey,
+    userId: req.session!.credentials.userId
   });
+});
 
-  authedRouter.get("/credentials", (req, res) => {
-    res.json({
-      email: req.session!.credentials.email,
-      password: req.session!.credentials.password,
-      apikey: req.session!.credentials.apikey,
-      userId: req.session!.credentials.userId
-    });
-  });
+authedRouter.post('/plaid/getAccessToken', express.json(), async (req, res) => {
+  if (!plaidClientId || !plaidSecret || !plaidPublicKey || !plaidEnvironment) {
+    console.error('Missing Plaid configuration');
+    res.status(500);
+    return res.json({error: 'Invalid Plaid configuration'});
+  }
+  const plaidClient = new plaid.Client(
+    plaidClientId,
+    plaidSecret,
+    plaidPublicKey,
+    plaid.environments[plaidEnvironment],
+    {version: '2019-05-29'},
+  );
 
-  authedRouter.post('/plaid/getAccessToken', express.json(), async (req, res) => {
-    if (!plaidClientId || !plaidSecret || !plaidPublicKey || !plaidEnvironment) {
-      console.error('Missing Plaid configuration');
+  const publicToken = req.body.publicToken;
+  if (!publicToken) {
+    res.status(400);
+    return res.json({error: 'Missing publicToken'});
+  }
+  const accountId = req.body.accountId;
+  if (!accountId) {
+    res.status(400);
+    return res.json({error: 'Missing accountId'});
+  }
+  
+  plaidClient.exchangePublicToken(publicToken, async (error, tokenResponse) => {
+    if (error !== null) {
+      console.error(error);
       res.status(500);
-      return res.json({error: 'Invalid Plaid configuration'});
+      return res.json({error: 'Trouble linking the account with Plaid'});
     }
-    const plaidClient = new plaid.Client(
-      plaidClientId,
-      plaidSecret,
-      plaidPublicKey,
-      plaid.environments[plaidEnvironment],
-      {version: '2019-05-29'},
 
-    );
-
-    const publicToken = req.body.publicToken;
-    if (!publicToken) {
-      res.status(400);
-      return res.json({error: 'Missing publicToken'});
-    }
-    const accountId = req.body.accountId;
-    if (!accountId) {
-      res.status(400);
-      return res.json({error: 'Missing accountId'});
-    }
-    
-    plaidClient.exchangePublicToken(publicToken, async (error, tokenResponse) => {
-      if (error !== null) {
-        console.error(error);
-        res.status(500);
-        return res.json({error: 'Trouble linking the account with Plaid'});
-      }
-
-      const apollo = await mkApollo(process.env.HASURA_ADMIN_KEY!, true);
-      try {
-        await apollo.mutate({
-          mutation: gql`
-            mutation StorePlaidLink($access_token: String!, $item_id: String!, $user_id: String!, $account_id: String!) {
-              insert_plaid_links(objects: {
-                access_token: $access_token,
-                item_id: $item_id,
-                user_id: $user_id
-              }) {
-                returning {
-                  item_id
-                }
+    const apollo = await mkApollo(process.env.HASURA_ADMIN_KEY!, true);
+    try {
+      await apollo.mutate({
+        mutation: gql`
+          mutation StorePlaidLink($access_token: String!, $item_id: String!, $user_id: String!, $account_id: String!) {
+            insert_plaid_links(objects: {
+              access_token: $access_token,
+              item_id: $item_id,
+              user_id: $user_id
+            }) {
+              returning {
+                item_id
               }
-
-              update_accounts(_set: {plaid_item_id: $item_id}, where: {id: {_eq: $account_id}}) { returning {id} }
             }
-          `,
-          variables: {
-            access_token: tokenResponse.access_token,
-            item_id: tokenResponse.item_id,
-            user_id: req.userId,
-            account_id: accountId,
-          },
-        });
-      } catch (ex) {
-        console.error(ex);
-        res.status(500);
-        res.json({error: 'Could not store Plaid item into database'});
-      }
-      console.log(JSON.stringify(tokenResponse, null, 4));
-      res.json({});
-    });
-  })
 
-  app.use("/api", authedRouter);
-} else {
-  debug('Using PouchDB exclusively. No logins supported.');
-}
+            update_accounts(_set: {plaid_item_id: $item_id}, where: {id: {_eq: $account_id}}) { returning {id} }
+          }
+        `,
+        variables: {
+          access_token: tokenResponse.access_token,
+          item_id: tokenResponse.item_id,
+          user_id: req.userId,
+          account_id: accountId,
+        },
+      });
+    } catch (ex) {
+      console.error(ex);
+      res.status(500);
+      res.json({error: 'Could not store Plaid item into database'});
+    }
+    console.log(JSON.stringify(tokenResponse, null, 4));
+    res.json({});
+  });
+})
 
+app.use("/api", authedRouter);
 
 // Serve static files for React
 app.use(express.static(path.join(__dirname, "../../../client", "public")));

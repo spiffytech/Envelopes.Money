942b109869e164429f8c8a0ee21bd78e16d3a278
spiffytech <spiffy@spiffy.tech>
2019-12-17 20:48:07 -0500

    Tweaked text size

--- client/src/views/Transactions/Transaction.svelte
+++ client/src/views/Transactions/Transaction.svelte
@@ -9,14 +9,14 @@
   style="display: contents"
   href={`/editTxn/${encodeURIComponent(encodeURIComponent(libtxngroup.txnId(txn)))}`}>
   <div class="border rounded-lg p-3">
-    <p>{libtxngroup.date(txn)}</p>
+    <p class="text-sm">{libtxngroup.date(txn)}</p>
 
     <p class="flex justify-between">
       <span>{libtxngroup.labelOrLabel(txn)}</span>
-      <span>{toDollars(libtxngroup.amount(txn))}</span>
+      <span class="text-sm">{toDollars(libtxngroup.amount(txn))}</span>
     </p>
 
-    <p style="text-overflow: ellipsis" class="whitespace-no-wrap overflow-hidden">
+    <p style="text-overflow: ellipsis" class="whitespace-no-wrap overflow-hidden text-sm">
       <span
       title={libtxngroup.fromName(txn) + ', ' + libtxngroup.toNames(txn).join(', ')}
       >{libtxngroup.fromName(txn)} &rightleftarrows; {libtxngroup.toNames(txn).join(', ')}</span>

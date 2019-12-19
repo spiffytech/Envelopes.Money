ca1b2f69c12ce51683d849f093fdabdd7e8c0cce
spiffytech <spiffy@spiffy.tech>
2019-12-18 22:18:06 -0500

    Add TopBar to the Fills page

--- client/src/FillEnvelopes.svelte
+++ client/src/FillEnvelopes.svelte
@@ -4,6 +4,8 @@
   import * as shortid from 'shortid';
   import { getContext } from 'svelte';
 
+  import TopBar from './components/material/TopBar.svelte';
+
   import Balance from './Balance.svelte';
   import * as Balances from './lib/Balances';
   import { toDollars } from './lib/pennies';
@@ -23,6 +25,8 @@
   const fillsObj = new fromPairs(
     envelopes.map(envelope => [envelope.id, null])
   );
+
+  $: console.log(envelopes);
 
   $: unallocated = envelopes.find(
     envelope => envelope.name === '[Unallocated]'
@@ -110,73 +114,75 @@
   }
 </script>
 
-<form class="content" on:submit|preventDefault={handleSubmit}>
-  <div class="flex flex-wrap">
-    <div>
-      <button class="btn btn-primary" on:click|preventDefault={() => magicFill($balancesStore)}>Magic Fill</button>
-      <p class="italic text-xs">Fills all envelopes. If an envelope is below zero, raises it to the fill amount.</p>
+<TopBar title="Fill Envelopes">
+  <form class="content" on:submit|preventDefault={handleSubmit}>
+    <div class="flex flex-wrap">
+      <div>
+        <button class="btn btn-primary" on:click|preventDefault={() => magicFill($balancesStore)}>Magic Fill</button>
+        <p class="italic text-xs">Fills all envelopes. If an envelope is below zero, raises it to the fill amount.</p>
+      </div>
+
+      <input type="submit" value="Save Fills" class="btn btn-primary" />
     </div>
+
+    <p class="sticky top-0 bg-white shadow-md p-3 mb-3">New [Unallocated] balance: {toDollars($balancesStore[unallocated.id] - sumOfFills)}</p>
+
+    <select
+      bind:value={$intervalStore}
+      on:change={event => intervalStore.set(event.target.value)}
+      class="border">
+      <option value="weekly">Weekly</option>
+      <option value="biweekly">Biweekly</option>
+      <option value="bimonthly">Bimonthly</option>
+      <option value="monthly">Monthly</option>
+      <option value="annually">Annually</option>
+    </select>
+
+    {#each envelopes as envelope (envelope.id)}
+      <div class="flex flex-wrap">
+        <div style="flex-basis: 450px;">
+          <Balance account={envelopesMap.get(envelope.id)} defaultDaysToRender={15} />
+          <p class="text-xs p-3">{toDollars($balancesStore[envelope.id])} + {toDollars(fillsByAccount.get(envelope.id))} = {toDollars($balancesStore[envelope.id] + fillsByAccount.get(envelope.id))}</p>
+        </div>
+        <div>
+
+          <label>
+            <input
+              type="radio"
+              bind:group={fillsObj[envelope.id]}
+              value="fill-with-amount" />
+            Fill with {toDollars(calcFillAmount(envelope))} ({$intervalStore} amount)
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              bind:group={fillsObj[envelope.id]}
+              value="set-to-amount" />
+            Raise/lower to {toDollars(calcFillAmount(envelope))} ({$intervalStore}
+            amount)
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              bind:group={fillsObj[envelope.id]}
+              value="set-to-zero" />
+            Set to 0.00
+          </label>
+
+          <br />
+
+          <label>
+            Other amount
+            <input class="border" type="number" step="0.01" on:input={event => event.target.value ? fillsObj[envelope.id] = parseFloat(event.target.value) : fillsObj[envelope.id] = null} />
+          </label>
+        </div>
+      </div>
+
+      <hr />
+    {/each}
 
     <input type="submit" value="Save Fills" class="btn btn-primary" />
-  </div>
-
-  <p class="sticky top-0 bg-white shadow-md p-3 mb-3">New [Unallocated] balance: {toDollars($balancesStore[unallocated.id] - sumOfFills)}</p>
-
-  <select
-    bind:value={$intervalStore}
-    on:change={event => intervalStore.set(event.target.value)}
-    class="border">
-    <option value="weekly">Weekly</option>
-    <option value="biweekly">Biweekly</option>
-    <option value="bimonthly">Bimonthly</option>
-    <option value="monthly">Monthly</option>
-    <option value="annually">Annually</option>
-  </select>
-
-  {#each envelopes as envelope (envelope.id)}
-    <div class="flex flex-wrap">
-      <div style="flex-basis: 450px;">
-        <Balance account={envelopesMap.get(envelope.id)} defaultDaysToRender={15} />
-        <p class="text-xs p-3">{toDollars($balancesStore[envelope.id])} + {toDollars(fillsByAccount.get(envelope.id))} = {toDollars($balancesStore[envelope.id] + fillsByAccount.get(envelope.id))}</p>
-      </div>
-      <div>
-
-        <label>
-          <input
-            type="radio"
-            bind:group={fillsObj[envelope.id]}
-            value="fill-with-amount" />
-          Fill with {toDollars(calcFillAmount(envelope))} ({$intervalStore} amount)
-        </label>
-        <br />
-        <label>
-          <input
-            type="radio"
-            bind:group={fillsObj[envelope.id]}
-            value="set-to-amount" />
-          Raise/lower to {toDollars(calcFillAmount(envelope))} ({$intervalStore}
-          amount)
-        </label>
-        <br />
-        <label>
-          <input
-            type="radio"
-            bind:group={fillsObj[envelope.id]}
-            value="set-to-zero" />
-          Set to 0.00
-        </label>
-
-        <br />
-
-        <label>
-          Other amount
-          <input class="border" type="number" step="0.01" on:input={event => event.target.value ? fillsObj[envelope.id] = parseFloat(event.target.value) : fillsObj[envelope.id] = null} />
-        </label>
-      </div>
-    </div>
-
-    <hr />
-  {/each}
-
-  <input type="submit" value="Save Fills" class="btn btn-primary" />
-</form>
+  </form>
+</TopBar>

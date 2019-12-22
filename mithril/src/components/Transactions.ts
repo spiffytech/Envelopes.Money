f5a64f9e8a0ed3cd67994021060de07f50b7aa53
spiffytech <spiffy@spiffy.tech>
2019-12-22 12:47:50 -0500

    Fetch txn_groups instead of transactions

--- mithril/src/components/Transactions.ts
+++ mithril/src/components/Transactions.ts
@@ -21,14 +21,18 @@ interface TransactionsSchema {
 }
 
 type TransactionsEvent =
-  | { type: 'start'; hasura: Hasura, creds: any }
+  | { type: 'initialize'; hasura: Hasura; creds: any }
   | { type: 'configure' }
-  | { type: 'ready', transactions?: any[]};
+  | { type: 'ready'; transactions?: any[] };
 
 interface TransactionsContext {
   loaded: boolean;
   hasura: Hasura | null;
-  urlParams: {envelope: string | null; account: string | null, searchTerm: string | null};
+  urlParams: {
+    envelope: string | null;
+    account: string | null;
+    searchTerm: string | null;
+  };
   transactions: any[];
 }
 
@@ -37,78 +41,114 @@ export default function Transactions(): m.Component<LayoutChildProps> {
     TransactionsContext,
     TransactionsSchema,
     TransactionsEvent
-  >(
-    {
-      id: 'transactions',
-      initial: 'idle',
-      context: {
-        loaded: false,
-        hasura: null,
-        urlParams: {envelope: null, account: null, searchTerm: null},
-        transactions: [],
+  >({
+    id: 'transactions',
+    initial: 'idle',
+    context: {
+      loaded: false,
+      hasura: null,
+      urlParams: { envelope: null, account: null, searchTerm: null },
+      transactions: [],
+    },
+    states: {
+      idle: {
+        on: {
+          initialize: {
+            target: 'unconfigured',
+            actions: assign((context, event) => ({
+              ...context,
+              hasura: event.hasura,
+              creds: event.creds,
+            })),
+          },
+        },
       },
-      states: {
-        idle: {
-          on: {
-            start: {
-              target: 'unconfigured',
-              actions: assign((context, event) => ({...context, hasura: event.hasura, creds: event.creds })),
+      unconfigured: {
+        invoke: {
+          src: () => Promise.resolve(m.route.param()),
+          onDone: {
+            target: 'configured',
+            actions: assign((context, event) => ({
+              ...context,
+              urlParams: event.data,
+            })),
+          },
+        },
+      },
+      configured: {
+        initial: 'loading',
+        on: { configure: 'unconfigured' },
+        states: {
+          loading: {
+            entry: assign(context => ({
+              ...context,
+              loaded: false,
+              transactions: [],
+            })),
+            on: {
+              ready: {
+                target: 'ready',
+                actions: assign((context, event) => ({
+                  ...context,
+                  transactions: event.transactions,
+                  loaded: true,
+                })),
+              },
+            },
+          },
+          ready: {
+            on: {
+              ready: {
+                target: 'ready',
+                actions: assign((context, event) => ({
+                  ...context,
+                  transactions: event.transactions,
+                })),
+              },
             },
           },
         },
-        unconfigured: {
-          invoke: {
-            src: () => Promise.resolve(m.route.param()),
-            onDone: {
-              target: 'configured',
-              actions: assign((context, event) => ({...context, urlParams: event.data })),
-            },
-          },
-        },
-        configured: {
-          initial: 'loading',
-          on: { configure: 'unconfigured' },
-          states: {
-            loading: {
-              entry: assign((context) => ({...context, loaded: false, transactions: []})),
-              on: {
-                ready: {
-                  target: 'ready',
-                  actions: assign((context, event) => ({...context, transactions: event.transactions, loaded: true}))
-                },
-              }
-            },
-            ready: {
-              on: {
-                ready: {
-                  target: 'ready',
-                  actions: assign((context, event) => ({...context, transactions: event.transactions}))
-                },
-              }
-            },
-          },
-          invoke: {
-            src: ({hasura, creds}: {hasura: Hasura, creds: any}) => (fireEvent) => {
-              const {unsubscribe} = hasura.subscribe({
+        invoke: {
+          src: ({
+            hasura,
+            creds,
+          }: {
+            hasura: Hasura;
+            creds: any;
+          }) => fireEvent => {
+            const { unsubscribe } = hasura.subscribe(
+              {
                 query: gql`
                   ${fragments}
-                  subscription SubscribeTransactions($user_id: String!) {
-                    transactions(where: { user_id: { _eq: $user_id } }) {
-                      ...transaction
+                  subscription SubscribeTransactions(
+                    $user_id: String!
+                    $date: date
+                    $txn_id: String
+                  ) {
+                    txns_grouped(
+                      where: {
+                        _or: { date: { _lte: $date } }
+                        _and: { date: { _lt: $date }, txn_id: { _lt: $txn_id } }
+                      }
+                      order_by: { date: desc, txn_id: asc }
+                      limit: 50
+                    ) {
+                      ...txn_grouped
                     }
                   }
                 `,
-                variables: {user_id: creds.userId}
-              }, ({data}) => {
-                fireEvent({type: 'ready', transactions: data.transactions});
-              });
-              return unsubscribe;
-            }
-          }
+                variables: { user_id: creds.userId },
+              },
+              ({ data }) => {
+                fireEvent({ type: 'ready', transactions: data.txns_grouped });
+              }
+            );
+            return unsubscribe;
+          },
         },
       },
-    }
-  );
+    },
+  });
 
   let context: TransactionsContext = {
     loaded: false,
@@ -116,12 +156,12 @@ export default function Transactions(): m.Component<LayoutChildProps> {
     urlParams: {
       account: null,
       envelope: null,
-      searchTerm: null
+      searchTerm: null,
     },
     transactions: [],
-  }
+  };
   const service = interpret(machine);
-  service.onChange((newContext) => {
+  service.onChange(newContext => {
     context = newContext;
     m.redraw();
   });
@@ -130,7 +170,7 @@ export default function Transactions(): m.Component<LayoutChildProps> {
     oninit({ attrs: { setTitle, hasura, creds } }) {
       setTitle('Transactions');
       service.start();
-      service.send('start', { hasura, creds });
+      service.send('initialize', { hasura, creds });
     },
 
     view() {
@@ -139,7 +179,9 @@ export default function Transactions(): m.Component<LayoutChildProps> {
       }
 
       return [
-        context.transactions.map(transaction => m('', JSON.stringify(transaction)))
+        context.transactions.map(transaction =>
+          m('', JSON.stringify(transaction))
+        ),
       ];
     },
   };
